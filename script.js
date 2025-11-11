2307945f4f29150bdb2fdfea1712d33180206820
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2025-11-11 22:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
+	event.preventDefault();
+	
+	let r=Number(document.getElementById("radius").value);
+	let vol = (4/3) * Math.PI * Math.pow(r, 3);
+	document.getElementById("volume").value=vol;
   
-} 
+};
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
